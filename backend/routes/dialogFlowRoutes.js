bba2b0f75ed0a38144ffb64f13c36f8e3e3f78e1
Nikhil <nikhilownstheworld@gmail.com>
2020-01-25 22:12:37 +0530

    structuring prescription into json

--- backend/routes/dialogFlowRoutes.js
+++ backend/routes/dialogFlowRoutes.js
@@ -1,5 +1,7 @@
 const voicePrescription = require('../src/voicePrescription')
 
+const structurePrescription = require('../src/structurePrescription')
+
 module.exports = app => {
 
     app.get('/', (req, res) => {
@@ -9,7 +11,8 @@ module.exports = app => {
     app.post('/api/df_text_query', async (req, res) => {
 
         let responses = await voicePrescription.textQuery(req.body.text, req.body.parameters);
-        res.json(responses[0].queryResult)
+        const prescription = await structurePrescription(responses[0].queryResult)
+        res.json(prescription)
     });
 
     app.post('/api/df_event_query', async (req, res) => {
