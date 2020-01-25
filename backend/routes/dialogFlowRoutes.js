e93d8d7389bd734db9b5f0dc6ac042671ddb1580
Nikhil <nikhilownstheworld@gmail.com>
2020-01-25 23:52:24 +0530

    pdf generator added

--- backend/routes/dialogFlowRoutes.js
+++ backend/routes/dialogFlowRoutes.js
@@ -23,24 +23,8 @@ module.exports = app => {
             const now = new Date();
 
     let currentdate = "" + now.getDate() + "-" + (now.getMonth() + 1) + "-" + now.getFullYear();
-        const data = {
-            title: "Medical Report",
-            doctor: "Dr. Deep Maheshwari",
-            hospital: "Apollo Hospital",
-            doctorNumber: "6302734859",
-            date: currentdate,
-            name: "Tapish",
-            age: 19,
-            sex: "male",
-            symptoms: ["Dry Cough for last 3 days ", "fever ", "Running Nose"],
-            diagnosis: ["Acute Bronchitis"],
-            prescription: [{ name: "Paracetamol", Strength: "500 mg", Dosage: "once a day for 3 days" },
-            { name: "Dolo", Strength: "650 mg", Dosage: "once a day for 3 days" }
-            ],
-            advice: ["Drink Warm Water ", "Dont eat grapes"]
-        }
-
-
+        const data = req.body.data
+        
         await createPDF.createPDF(data)
         res.send("Successfull")
     })
