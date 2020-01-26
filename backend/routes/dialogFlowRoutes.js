99ddba9a972a80d5d3fbf04630442fb37b3bbac8
Krishnendu Sudheesh <krishnendusudheesh4@gmail.com>
2020-01-26 09:39:11 +0530

    accidental backend change

--- backend/routes/dialogFlowRoutes.js
+++ backend/routes/dialogFlowRoutes.js
@@ -1,31 +1,36 @@
-const voicePrescription = require('../src/voicePrescription')
+const voicePrescription = require("../src/voicePrescription");
 
-const createPDF = require('../src/pdfGenerator')
+const createPDF = require("../src/pdfGenerator");
 
 module.exports = app => {
+  app.get("/", (req, res) => {
+    res.send({ HELLO: "FROM JARVIS" });
+  });
 
-    app.get('/', (req, res) => {
-        res.send({'HELLO': 'FROM JARVIS'})
-    });
+  app.post("/api/df_text_query", async (req, res) => {
+    let responses = await voicePrescription.textQuery(
+      req.body.text,
+      req.body.parameters
+    );
+    res.json(responses[0].queryResult);
+  });
 
-    app.post('/api/df_text_query', async (req, res) => {
+  app.post("/api/df_event_query", async (req, res) => {
+    let responses = await voicePrescription.eventQuery(
+      req.body.event,
+      req.body.parameters
+    );
+    res.json(responses[0].queryResult);
+  });
 
-        let responses = await voicePrescription.textQuery(req.body.text, req.body.parameters);
-        res.json(responses[0].queryResult)
-    });
+  app.post("/api/finalData", async (req, res) => {
+    const now = new Date();
 
-    app.post('/api/df_event_query', async (req, res) => {
-        let responses = await voicePrescription.eventQuery(req.body.event, req.body.parameters);
-        res.json(responses[0].queryResult)
-    });
+    let currentdate =
+      "" + now.getDate() + "-" + (now.getMonth() + 1) + "-" + now.getFullYear();
+    const data = req.body.data;
 
-    app.post('/api/finalData', async (req, res) => {
-            const now = new Date();
-
-    let currentdate = "" + now.getDate() + "-" + (now.getMonth() + 1) + "-" + now.getFullYear();
-        const data = req.body.data
-        
-        await createPDF.createPDF(data)
-        res.send("Successfull")
-    })
-}
+    await createPDF.createPDF(data);
+    res.send("Successfull");
+  });
+};
